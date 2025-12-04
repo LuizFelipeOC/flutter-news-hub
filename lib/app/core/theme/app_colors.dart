47325a471cc482434b295f7018440da5392093cb
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2025-12-03 21:34:59 -0400

    chore: add green400 color

--- lib/app/core/theme/app_colors.dart
+++ lib/app/core/theme/app_colors.dart
@@ -4,4 +4,6 @@ final class AppColors {
   static const neutral100 = Color(0xFFf5f5f5);
   static const neutral800 = Color(0xFF262626);
   static const neutral900 = Color(0xFF171717);
+
+  static const green400 = Color(0xFF4ade80);
 }
