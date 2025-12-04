4ef5051396a494a1e2e4c278eb35f5b2188674bc
_costa <luizfelipeeoliveiraac@gmail.com>
2025-12-03 21:04:28 -0400

    init project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NewsHub());
+}
+
+class NewsHub extends StatelessWidget {
+  const NewsHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
