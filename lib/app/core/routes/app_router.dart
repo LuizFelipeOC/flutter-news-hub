ee4c7ec5c5197ca5a03f96aa31dd47759ba961fa
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2025-12-03 21:34:35 -0400

    refactor: configure onboarding screen

--- lib/app/core/routes/app_router.dart
+++ lib/app/core/routes/app_router.dart
@@ -1,6 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:news_hub/app/pages/onboarding/onboarding_page.dart';
 
-final GoRouter routerConfiguration = GoRouter(routes: <RouteBase>[
-   
+final GoRouter routerConfiguration = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return OnboardingPage(key: ValueKey('Onboarding'));
+      },
+    ),
   ],
 );
