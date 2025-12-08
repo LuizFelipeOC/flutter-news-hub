cc3d913234efe40179658cbb84cfe528f82181a9
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2025-12-08 00:06:25 -0400

    chore: add new neutral colors

--- lib/app/core/theme/app_colors.dart
+++ lib/app/core/theme/app_colors.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 final class AppColors {
+  static const neutral50 = Color(0xFFfafafa);
   static const neutral100 = Color(0xFFf5f5f5);
+  static const neutral200 = Color(0xFFe5e5e5);
+  static const neutral300 = Color(0xFFd4d4d4);
+  static const neutral400 = Color(0xFFa3a3a3);
   static const neutral800 = Color(0xFF262626);
   static const neutral900 = Color(0xFF171717);
 
