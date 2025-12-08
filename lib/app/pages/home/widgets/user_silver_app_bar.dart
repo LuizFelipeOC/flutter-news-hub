ba4917766f84afe390d6c39d2737a67789aaf9cc
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2025-12-08 14:20:57 -0400

    chore: move models folder to data folder

--- lib/app/pages/home/widgets/user_silver_app_bar.dart
+++ lib/app/pages/home/widgets/user_silver_app_bar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:news_hub/app/domain/models/user_model.dart';
+import 'package:news_hub/app/data/domain/models/user_model.dart';
 import 'package:news_hub/app/pages/login/login_page.dart';
 
 class UserSilverAppBar extends StatelessWidget {
