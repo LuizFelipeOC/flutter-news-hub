d798993d09b095a4d3739384a830e36481b0920f
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2026-03-10 20:58:46 -0400

    fix: update default selected category and reorder categories in CategoriesFilterController

--- lib/app/core/widgets/categories_filter/categories_filter_controller.dart
+++ lib/app/core/widgets/categories_filter/categories_filter_controller.dart
@@ -3,12 +3,12 @@ import 'package:news_hub/app/data/models/filter_model/categories_filter_model.da
 
 class CategoriesFilterController {
   ValueNotifier<CategoriesFilterModel> selectedCategory = ValueNotifier(
-    CategoriesFilterModel(id: 'relevant', name: 'Relevantes'),
+    CategoriesFilterModel(id: 'new', name: 'Novos'),
   );
 
   List<CategoriesFilterModel> categories = [
-    CategoriesFilterModel(id: 'relevant', name: 'Relevantes'),
     CategoriesFilterModel(id: 'new', name: 'Novos'),
+    CategoriesFilterModel(id: 'relevant', name: 'Relevantes'),
     CategoriesFilterModel(id: 'old', name: 'Velhos'),
   ];
 }
